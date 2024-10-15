c548996d13949a14bdd88340b0e432c1a047aa34
kikye04040 <kikye04040@gmail.com>
2024-10-15 11:12:09 +0900

    feat(board): 엔티티 구현
    Signed-off-by: kikye04040 <kikye04040@gmail.com>

--- src/main/java/com/sparta/springtrello/domain/board/entity/BoardEntity.java
+++ src/main/java/com/sparta/springtrello/domain/board/entity/BoardEntity.java
@@ -1,18 +1,21 @@
 package com.sparta.springtrello.domain.board.entity;
 
+import com.sparta.springtrello.domain.common.entity.Timestamped;
+import com.sparta.springtrello.domain.list.entity.ListEntity;
+import com.sparta.springtrello.domain.user.entity.UserEntity;
+import com.sparta.springtrello.domain.workspace.entity.WorkspaceEntity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Entity
 @NoArgsConstructor
 @Table(name = "boards")
-public class Board {
+public class BoardEntity extends Timestamped {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -29,21 +32,13 @@ public class Board {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "workspace_id", nullable = false)
-    private Workspace workspace;
+    private WorkspaceEntity workspace;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "created_by")
-    private User createdBy;
-
-    @Column(name = "created_at", nullable = false, updatable = false)
-    @CreationTimestamp
-    private Timestamp createdAt;
-
-    @Column(name = "updated_at", nullable = false)
-    @UpdateTimestamp
-    private Timestamp updatedAt;
+    private UserEntity createdBy;
 
     // 관계 설정: Board는 여러 List를 가질 수 있음
     @OneToMany(mappedBy = "board", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<List> lists = new ArrayList<>();
+    private List<ListEntity> lists = new ArrayList<>();
 }
