609f99236785737a97a486b9fcbf8bc319fd42c2
vege4944 <vege4944@gmail.com>
2024-10-16 15:00:08 +0900

    feat(member): MemberEntity 구현

--- src/main/java/com/sparta/springtrello/domain/member/entity/MemberEntity.java
+++ src/main/java/com/sparta/springtrello/domain/member/entity/MemberEntity.java
@@ -1,17 +1,17 @@
 package com.sparta.springtrello.domain.member.entity;
 
 import com.sparta.springtrello.domain.common.entity.Timestamped;
-import com.sparta.springtrello.domain.member.enums.MemberRoleType;
+import com.sparta.springtrello.domain.member.enums.MemberRole;
 import com.sparta.springtrello.domain.user.entity.UserEntity;
 import com.sparta.springtrello.domain.workspace.entity.WorkspaceEntity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.security.core.userdetails.User;
 
 @Getter
 @NoArgsConstructor
 @Entity
+@Table(name = "members")
 public class MemberEntity extends Timestamped {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,7 +19,7 @@ public class MemberEntity extends Timestamped {
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
-    private MemberRoleType memberRole;
+    private MemberRole memberRole;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
@@ -29,10 +29,13 @@ public class MemberEntity extends Timestamped {
     @JoinColumn(name = "workspace_id", nullable = false)
     private WorkspaceEntity workspace;
 
-    public MemberEntity (UserEntity user, WorkspaceEntity workspace, MemberRoleType memberRole) {
+    public MemberEntity (UserEntity user, WorkspaceEntity workspace, MemberRole memberRole) {
         this.user = user;
         this.workspace = workspace;
         this.memberRole = memberRole;
     }
-
+    
+    public void changeRole(MemberRole memberRole){
+        this.memberRole = memberRole;
+    }
 }
