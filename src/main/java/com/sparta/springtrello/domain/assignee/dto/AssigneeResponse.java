92adc9ef35d45e306cbfe596b475af9d5a696d9b
KTJ-1650 <xowns1650@gmail.com>
2024-10-16 15:06:15 +0900

    fix(assignee): DTO 수정

--- src/main/java/com/sparta/springtrello/domain/assignee/dto/AssigneeResponse.java
+++ src/main/java/com/sparta/springtrello/domain/assignee/dto/AssigneeResponse.java
@@ -3,11 +3,20 @@ package com.sparta.springtrello.domain.assignee.dto;
 import com.sparta.springtrello.domain.assignee.entity.AssigneeEntity;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class AssigneeResponse {
     private Long id;
+    private String username;
+    private String message;
+    private LocalDateTime assignedAt;
 
-    public AssigneeResponse(AssigneeEntity assigneeEntity) {
+
+    public AssigneeResponse(AssigneeEntity assigneeEntity,String message) {
         this.id = assigneeEntity.getId();
+        this.username = assigneeEntity.getUser().getUsername();
+        this.message = message;
+        this.assignedAt = assigneeEntity.getAssignedAt();
     }
 }
