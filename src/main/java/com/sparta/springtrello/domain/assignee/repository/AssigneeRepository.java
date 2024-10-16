765637bc5f58d10e64ca20626371319365a6c2b6
KTJ-1650 <xowns1650@gmail.com>
2024-10-16 15:05:18 +0900

    fix(assignee): 레포지토리 수정

--- src/main/java/com/sparta/springtrello/domain/assignee/repository/AssigneeRepository.java
+++ src/main/java/com/sparta/springtrello/domain/assignee/repository/AssigneeRepository.java
@@ -5,10 +5,15 @@ import com.sparta.springtrello.domain.card.entity.CardEntity;
 import com.sparta.springtrello.domain.user.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AssigneeRepository extends JpaRepository<AssigneeEntity,Long> {
     Optional<AssigneeEntity> findByIdAndCardId(Long assigneeId,Long cardId);
 
     boolean existsByCardAndUser(CardEntity card, UserEntity user);
+
+    Optional<AssigneeEntity> findByCardIdAndUserId(Long cardId, Long userId);
+
+    List<AssigneeEntity> findByCardId(Long cardId);
 }
