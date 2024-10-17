b1346a29bb61fe3adf56c02408eba06801f34d85
kikye04040 <kikye04040@gmail.com>
2024-10-17 12:53:14 +0900

    fix(board): S3 업로드 분리 구현 후 이미지 업로드 로직 수정
    Signed-off-by: kikye04040 <kikye04040@gmail.com>

--- src/main/java/com/sparta/springtrello/domain/board/controller/BoardImageController.java
+++ src/main/java/com/sparta/springtrello/domain/board/controller/BoardImageController.java
@@ -1,6 +1,7 @@
 package com.sparta.springtrello.domain.board.controller;
 
 import com.sparta.springtrello.domain.board.service.BoardImageService;
+import com.sparta.springtrello.domain.common.dto.UploadFileResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,21 +10,25 @@ import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/boards")
+@RequestMapping("/boards/images")
 public class BoardImageController {
 
     private final BoardImageService boardImageService;
 
-    @PostMapping("/upload")
-    public ResponseEntity<String> uploadProfileImage(@RequestParam("image") MultipartFile image) {
-        String imageUrl = boardImageService.upload(image);
-        return new ResponseEntity<>(imageUrl, HttpStatus.OK);
+    // 이미지 업로드 메서드
+    @PostMapping
+    public ResponseEntity<UploadFileResponse> uploadBoardImage(@RequestParam("image") MultipartFile image) {
+        // 업로드된 파일의 메타데이터 응답
+        UploadFileResponse uploadFileResponse = boardImageService.upload(image);
+        return new ResponseEntity<>(uploadFileResponse, HttpStatus.OK);
     }
 
-    @DeleteMapping("/delete")
-    public ResponseEntity<String> deleteProfileImage(@RequestParam("imageUrl") String imageUrl) {
-        boardImageService.deleteImageFromS3(imageUrl);
-        return new ResponseEntity<>("Image deleted successfully", HttpStatus.OK);
+    // 이미지 삭제 메서드
+    @DeleteMapping
+    public ResponseEntity<String> deleteBoardImage(@RequestParam("imageUrl") String imageUrl) {
+        // 이미지 삭제 처리
+        boardImageService.deleteBackgroundImage(imageUrl);
+        return new ResponseEntity<>("이미지가 성공적으로 삭제되었습니다.", HttpStatus.OK);
     }
 }
 
