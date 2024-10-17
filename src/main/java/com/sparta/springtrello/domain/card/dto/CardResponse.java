f91955ea445a989516bbb97906058a7e876c6e78
kikye04040 <kikye04040@gmail.com>
2024-10-17 16:00:06 +0900

    fix(card): CommentCardResponse 설정 후 Card 전체 리팩토링
    Signed-off-by: kikye04040 <kikye04040@gmail.com>

--- src/main/java/com/sparta/springtrello/domain/card/dto/CardResponse.java
+++ src/main/java/com/sparta/springtrello/domain/card/dto/CardResponse.java
@@ -5,12 +5,17 @@ import com.sparta.springtrello.domain.assignee.entity.AssigneeEntity;
 import com.sparta.springtrello.domain.card.entity.CardEntity;
 import com.sparta.springtrello.domain.comment.dto.CommentCardResponse;
 import com.sparta.springtrello.domain.comment.entity.CommentEntity;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CardResponse{
     private String title;
     private String description;
@@ -18,11 +23,13 @@ public class CardResponse{
     private String message;
     private List<AssigneeResponse> assigneeResponseList;
     private List<CommentCardResponse> commentResponseList;
+
     public CardResponse(String title,String description, LocalDate dueDate){
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
     }
+
     public CardResponse(CardEntity card, List<AssigneeEntity> assigneeEntityList, String message, List<CommentEntity> commentEntityList) {
         this.title = card.getTitle();
         this.description = card.getDescription();
@@ -32,9 +39,10 @@ public class CardResponse{
                 .map(assignee -> new AssigneeResponse(assignee, message))
                 .toList();
         this.commentResponseList = commentEntityList.stream()
-                .map(comment -> new CommentCardResponse(comment))
+                .map(CommentCardResponse::new)
                 .toList();
     }
+
     public void message(String message) {
         this.message = message;
     }
